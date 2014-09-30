0db18fb3702eba9eae471e54204ad803b9c4d28a
Maxwell Rebo <maxwell.b.rebo@gmail.com>
2014-09-30 09:42:42 -0400

    Nodejs or browser

--- swiss.js
+++ swiss.js
@@ -1,61 +1,63 @@
 //Swiss.js: Odds and ends utilities in JavaScript
 
-var Swiss = function(){
-	this.version = "0.0.1";
-};
+(function () {
+	var rt = this;
 
-Swiss.prototype.isObject = function(thing){
-	return (Object.prototype.toString.call(thing)==='[object Object]');
-};
-
-Swiss.prototype.isArray = function(thing){
-	return (Object.prototype.toString.call(thing)==='[object Array]');
-};
-
-//Adds the 'append' function, a la Python syntax 
-//***WARNING*** Modifies array prototype
-Swiss.prototype.addAppendFunction = function(){
-	Array.prototype.append = Array.prototype.push;
-};
-
-//Adds the 'last' function, which simply returns the last element of the array in question
-//***WARNING*** Modifies array prototype
-Swiss.prototype.addLastFunction = function(){
-	Array.prototype.last = function(){
-		return this[this.length - 1];
+	var Swiss = function(){
+		this.version = "0.0.1";
 	};
-};
 
-//Convenience method to add only the Array helpers
-Swiss.prototype.addAllArrayHelpers = function(){
-	this.addAppendFunction();
-	this.addLastFunction();
-};
+	Swiss.prototype.isObject = function(thing){
+		return (Object.prototype.toString.call(thing)==='[object Object]');
+	};
 
-Swiss.prototype.addAllObjectHelpers = function(){
-	//add object helpers
-};
+	Swiss.prototype.isArray = function(thing){
+		return (Object.prototype.toString.call(thing)==='[object Array]');
+	};
 
-//Convenience method to add all helpers to Object and Array prototypes
-Swiss.prototype.addAll = function(){
-	this.addAllArrayHelpers()
-	this.addAllObjectHelpers();
-};
+	//Adds the 'append' function, a la Python syntax 
+	Swiss.prototype.addAppendFunction = function(){
+		Array.prototype.append = Array.prototype.push;
+	};
 
-Swiss.prototype.singleQuote = function(str){
-	return "\'" + str + "\'";
-};
+	//Adds the 'last' function, which simply returns the last element of the calling array
+	Swiss.prototype.addLastFunction = function(){
+		Array.prototype.last = function(){
+			return this[this.length - 1];
+		};
+	};
 
-Swiss.prototype.doubleQuote = function(str){
-	return '\"' + str + '\"';
-};
+	//Convenience method to add only the Array helpers
+	Swiss.prototype.addAllArrayHelpers = function(){
+		this.addAppendFunction();
+		this.addLastFunction();
+	};
 
-Swiss.prototype.isFalsy = function(val){ 
-	if (val=="false" || val==false || val==0 || val=="0" || val==null || !val){ return true }
-	else { return false } 
-};
+	Swiss.prototype.singleQuote = function(str){
+		return "\'" + str + "\'";
+	};
 
-Swiss.prototype.isTruthy = function(val){
-	if (val==true || val=="true" || val==1 || val=="1"){ return true }
-	else { return false }
-};
+	Swiss.prototype.doubleQuote = function(str){
+		return '\"' + str + '\"';
+	};
+
+	Swiss.prototype.isFalsy = function(val){ 
+		if (val=="false" || val==false || val==0 || val=="0" || val==null || !val){ return true }
+		else { return false } 
+	};
+
+	Swiss.prototype.isTruthy = function(val){
+		if (val==true || val=="true" || val==1 || val=="1"){ return true }
+		else { return false }
+	};
+
+	var swiss = new Swiss();
+
+	if (typeof module !== 'undefined' && module.exports) {
+            module.exports = swiss;
+            rt.swiss = swiss;
+    } else {
+            rt.swiss = swiss;
+    }
+
+})();
